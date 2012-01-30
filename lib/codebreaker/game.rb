ffda97146b74989570e5e988b57358ebc2fbf681
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-01-30 00:28:53 -0600

    used Extract Method refactoring to introduce abstractions that more clearly express the intent of the guess() method, creating the exact_match? and number_match? methods

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -5,12 +5,34 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
-      @output.puts ''
+      # use the Extract Method refactoring to introduce abstractions that more
+      # clearly express the intent of the guess() method. Here we replace the
+      # @secret[0] == guess[0] with the exact_match method to make the code
+      # more readable to the next person to stumble on this code
+      #
+      # if @secret[0] == guess[0]
+      if exact_match?(guess, 0)
+        mark = '+'
+      elsif number_match?( guess, 0)
+        mark = '-'
+      else
+        mark = ''
+      end
+      @output.puts mark
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?( guess[index] )
     end
   end
 end
