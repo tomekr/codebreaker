7db0a344586caca35b3450f6d5d8d28312169a55
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-01-31 22:45:09 -0600

    extracted out exact_match_count and number_match_count logic from the guess method into their own separate methods

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -17,20 +17,27 @@ module Codebreaker
       # more readable to the next person to stumble on this code
       #
       # if @secret[0] == guess[0]
-      mark = ''
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
+    end
 
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-
+    def number_match_count(guess)
+      number_match_count = 0
       (0..3).each do |index|
         if number_match?(guess, index)
-          mark << '-'
+          number_match_count += 1
         end
       end
-      @output.puts mark
+      number_match_count
+    end
+
+    def exact_match_count(guess)
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
 
     def exact_match?(guess, index)
